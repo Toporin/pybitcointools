50cfd494159b3565a2234ce0dacedbb7f5535347
Toporin <satochip.wallet@gmail.com>
2025-01-07 13:55:05 +0100

    Pybitcointools v0.3.0
     * remove old unmaintained code
     * Add support for asset listing with simplified API
     * Add polygon support

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='pycryptotools',
-    version='0.3.0.beta',
+    version='0.3.0',
     description='Python Crypto Coin Tools',
     long_description=open('README.md').read(),
     long_description_content_type= 'text/markdown',
